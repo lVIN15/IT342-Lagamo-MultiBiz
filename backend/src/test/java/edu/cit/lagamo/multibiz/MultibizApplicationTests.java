c2e63325ee15d0db4bf4d877b86cd96ced0e0cee
Lagamo MultiBiz <lagamo@multibiz.dev>
2026-03-09 10:41:22 +0800

    test(backend): add default MultibizApplicationTests Spring Boot test stub generated by Spring Initializr

---
+++ backend/src/test/java/edu/cit/lagamo/multibiz/MultibizApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.lagamo.multibiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultibizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
