3ba3e1442cbbf62297f7c3e58bbd4a5408440ceb
Lagamo MultiBiz <lagamo@multibiz.dev>
2026-03-28 04:09:30 +0800

    feat: MultibizApplication.java enabling core application features

--- backend/src/main/java/edu/cit/lagamo/multibiz/MultibizApplication.java
+++ backend/src/main/java/edu/cit/lagamo/multibiz/MultibizApplication.java
@@ -2,8 +2,10 @@ package edu.cit.lagamo.multibiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class MultibizApplication {
 
 	public static void main(String[] args) {
