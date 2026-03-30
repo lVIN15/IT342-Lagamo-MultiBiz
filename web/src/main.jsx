5f47c927b897dacdd8382d12782193cf607d2799
Lagamo MultiBiz <lagamo@multibiz.dev>
2026-03-30 09:02:47 +0800

    feat: web/src/main.jsx GoogleOAuthProvider wrapper

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="344480178647-hjd8mqk2k83ls2n020iaik37nv7ait25.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
