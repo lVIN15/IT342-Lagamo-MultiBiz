b346f79c6a1997b92cb1a6f2d9a540eb9883d745
Lagamo MultiBiz <lagamo@multibiz.dev>
2026-04-01 01:36:23 +0800

    feat(mobile): refactor HomeActivity to use Modular BottomNav routing

--- mobile/app/src/main/java/com/elvin/multibiz/HomeActivity.kt
+++ mobile/app/src/main/java/com/elvin/multibiz/HomeActivity.kt
@@ -2,11 +2,16 @@ package com.elvin.multibiz
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.elvin.multibiz.utils.setupBottomNav
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        // Wire up the reusable bottom navigation, highlight Home as active
+        val bottomNav = findViewById<android.view.View>(R.id.bottomNav)
+        setupBottomNav(this, bottomNav, R.id.nav_tab_home)
     }
 }
