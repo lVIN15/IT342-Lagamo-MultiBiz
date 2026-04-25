a267cc4594e33184af3e2f88755fc4ff4002598d
Lagamo MultiBiz <lagamo@multibiz.dev>
2026-04-25 15:44:38 +0800

    feat(backend): update TransactionRepository for report data retrieval

--- backend/src/main/java/edu/cit/lagamo/multibiz/repository/TransactionRepository.java
+++ backend/src/main/java/edu/cit/lagamo/multibiz/repository/TransactionRepository.java
@@ -4,6 +4,7 @@ import edu.cit.lagamo.multibiz.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -11,4 +12,6 @@ import java.util.UUID;
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
 
     List<Transaction> findByBusinessId(UUID businessId);
+
+    List<Transaction> findByBusinessIdAndCreatedAtBetween(UUID businessId, LocalDateTime start, LocalDateTime end);
 }
